ff2feefaf49f04c2748f0427658e60148bd6a17c
Marcos Batista <batista.vinicius09@gmail.com>
2022-01-12 17:50:48 -0300

    Atividade do Senai
    Atividade em JS do senai.

---
+++ pecas.js
@@ -0,0 +1,22 @@
+//Variaveis nomeadas de acordo com respectivas rsponsabilidades
+
+let PesoPeca= 100;
+let nomeDaPeca= "Martelo" ;
+let NumeroPecas=9;
+//Estruturas
+
+if(PesoPeca < 100) {
+console.log("Digite a peça");
+} else {
+    console.log("recadastre um dentro do limite de peso");
+}
+if(NumeroPecas <10 ){
+    console.log("Produto cadastrado");
+} else{
+    console.log("Volume acima do suportado");
+}
+if(nomeDaPeca=nomeDaPeca.length>3){
+    console.log("Produto Cadastrado");
+}else{
+    console.log("Numero de caracteres insuficientes")
+}
